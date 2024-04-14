2df896c068ba6ca11917c68dc42a85c26ef1d427
Adrian Rogers <adritek@gmail.com>
2024-04-14 19:29:08 -0400

    add ops to get book reviews and added dummy reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+let booksArray = Object.values(books);
 
 public_users.post("/register", (req, res) => {
   //Write your code here
@@ -24,21 +25,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     let author = req.params.author;
-    let booksArray = Object.values(books);
     let filterByAuthor = booksArray.filter((book) => book.author === author);
     res.send(filterByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let filterByTitle = booksArray.filter((book) => book.title === title);
+    res.send(filterByTitle);
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
