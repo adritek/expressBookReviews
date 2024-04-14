fe0cb1305e305811e6f59b1df201f8995cad6085
Adrian Rogers <adritek@gmail.com>
2024-04-14 19:03:45 -0400

    add operation to GET books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,16 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
- });
+    let isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author;
+    let booksArray = Object.values(books);
+    let filterByAuthor = booksArray.filter((book) => book.author === author);
+    res.send(filterByAuthor);
 });
 
 // Get all books based on title
